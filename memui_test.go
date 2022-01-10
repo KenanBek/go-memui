292d59027511261f07924b148d5d09ec4ca2b5d0
Kanan Rahimov <mail@kenanbek.me>
2022-01-10 21:43:50 +0100

    refactor tests for Regsiter

--- memui_test.go
+++ memui_test.go
@@ -6,26 +6,49 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-type DummtType struct {
+type Dummy1 struct {
 	Name string
 }
 
+type Dummy2 struct {
+	Count int
+}
+
 func TestMemUI_Register(t *testing.T) {
-	type checks func(t *testing.T, mui *MemUI) bool
+	type checks func(t *testing.T, mui *MemUI)
 	tests := []struct {
 		name    string
 		args    []interface{}
 		wantErr bool
 		checks  checks
 	}{
-		{"add a pointer object", []interface{}{&DummtType{"Test"}}, false, func(t *testing.T, mui *MemUI) bool {
-			tn := "*memui.DummtType"
-			mo := mui.memObjects
-			return assert.Len(t, mo, 1) && assert.Contains(t, mo, tn) && assert.Len(t, mo[tn], 1)
-		}},
-		{"add a value object", []interface{}{DummtType{"Test"}}, true, func(t *testing.T, mui *MemUI) bool {
-			return assert.Len(t, mui.memObjects, 0)
-		}},
+		{
+			"add a pointer",
+			[]interface{}{&Dummy1{"Test"}},
+			false,
+			func(t *testing.T, mui *MemUI) {
+				assert.Len(t, mui.memObjects, 1)
+				assert.Contains(t, mui.memObjects, "*memui.Dummy1")
+			},
+		},
+		{
+			"add a value",
+			[]interface{}{Dummy1{"Test"}},
+			true,
+			func(t *testing.T, mui *MemUI) {
+				assert.Len(t, mui.memObjects, 0)
+			},
+		},
+		{
+			"add multiple objects",
+			[]interface{}{&Dummy1{"dummy2"}, &Dummy2{2}, Dummy2{2}},
+			true,
+			func(t *testing.T, mui *MemUI) {
+				assert.Len(t, mui.memObjects, 2)
+				assert.Len(t, mui.memObjects["*memui.Dummy1"], 1)
+				assert.Len(t, mui.memObjects["*memui.Dummy2"], 1)
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
